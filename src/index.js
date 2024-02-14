816f621746f6ab5c53b1afce2b0386c9f15ef000
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-02-14 19:08:32 +0000

    added-auth0-authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -8,7 +9,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <Auth0Provider
+    domain="dev-83rczzza30aq7qla.us.auth0.com"
+    clientId="1oTf32fb8t0PAyRZY9VsUiCUOSqr6Yxe"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <App />
+  </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
